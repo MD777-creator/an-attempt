8f290562ed4e10ba6645e1bee975279ede69a7f1
guWu <3111018605@qq.com>
2022-03-10 11:34:59 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 import App from './App'
-
+import {
+  $http
+} from "@escook/request-miniprogram"
+uni.$http = $http
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: "数据加载中..."
+  })
+}
+$http.afterRequest = function(options) {
+  uni.hideLoading()
+}
+uni.$showMsg = function(title = "数据加载失败！", duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: "none"
+  })
+}
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
   return {
